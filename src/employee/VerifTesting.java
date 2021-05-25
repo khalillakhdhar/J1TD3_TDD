497f462e3ee2ebae510f6815c0684ef2a685526d
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-05-25 15:00:56 +0100

    Update VerifTesting.java

--- src/employee/VerifTesting.java
+++ src/employee/VerifTesting.java
@@ -1,6 +1,7 @@
 package employee;
 
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -20,6 +21,14 @@ public class VerifTesting {
 	public void verifintervalle()
 	{
 		assertFalse(emplo.horaire(12));
+		
+	}
+	@Test
+	public void verifprix()
+	{
+		assertTrue(emplo.verifPH(23));
+		
+		
 		
 	}
 	
