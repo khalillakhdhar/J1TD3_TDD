050819bb27ce842fbbbc82026b3261a424b2ea7a
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-05-25 14:41:58 +0100

    testing

---
+++ src/employee/VerifTesting.java
@@ -0,0 +1,19 @@
+package employee;
+
+import org.junit.Before;
+
+public class VerifTesting {
+
+	ValidateEmployee emplo;
+	@Before
+	public void initilise()
+	{
+		
+		emplo=new ValidateEmployee();
+		
+	}
+	
+	
+	
+	
+}
